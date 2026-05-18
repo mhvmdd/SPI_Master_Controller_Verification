aab99b68a6e641548defa7bbfc1d1df0e8afefd6
ahmed naggar <2200306@eng.asu.edu.eg>
2026-05-18 17:36:37 +0300

    Adding apb_master_bfm , spi_slave_bfm (not final version)

--- harness/sv_only/tb/apb_master_bfm.sv
+++ harness/sv_only/tb/apb_master_bfm.sv
@@ -26,41 +26,69 @@ module apb_master_bfm (apb_if.master apb);
     localparam [7:0] INT_STAT = 8'h1C;
     localparam [7:0] DELAY    = 8'h20;
 
-    initial begin
+    localparam int TIMEOUT_CYCLES = 100;
+
+    task automatic drive_idle();
         apb.cb_master.psel    <= 1'b0;
         apb.cb_master.penable <= 1'b0;
         apb.cb_master.pwrite  <= 1'b0;
         apb.cb_master.paddr   <= '0;
         apb.cb_master.pwdata  <= '0;
+    endtask
+
+    initial begin
+        drive_idle();
     end
+
+    task automatic wait_ready(string task_name , input [7:0] addr);
+        int timeout;
+        timeout = 0;
+        do begin
+            @(apb.cb_master);
+                timeout++;
+
+            if(timeout >= TIMEOUT_CYCLES)
+                $fatal("[APB_BFM] Timeout for PREADY in %s addr=%h time=%0t", task_name , addr, $time);
+        end
+        while(!apb.cb_master.pready);
+    endtask
 
     task automatic apb_write(input [7:0] addr, input [31:0] data);
         @(apb.cb_master);
+        //Setup Phase
         apb.cb_master.psel    <= 1'b1;
         apb.cb_master.penable <= 1'b0;
         apb.cb_master.pwrite  <= 1'b1;
         apb.cb_master.paddr   <= addr;
         apb.cb_master.pwdata  <= data;
         @(apb.cb_master);
+        //Access Phase
         apb.cb_master.penable <= 1'b1;
-        do @(apb.cb_master); while (!apb.cb_master.pready);
-        apb.cb_master.psel    <= 1'b0;
-        apb.cb_master.penable <= 1'b0;
-        apb.cb_master.pwrite  <= 1'b0;
+        //do @(apb.cb_master); while (!apb.cb_master.pready);                   //Infinite waiting could hang simulation
+        wait_ready("apb_write" , addr);
+
+        $display("[APB_BFM][WRITE] addr=%h data=%h time=%0t", addr,data, $time);
+
+        drive_idle();
     endtask
 
     task automatic apb_read(input [7:0] addr, output [31:0] data);
         @(apb.cb_master);
+        // Setup Phase
         apb.cb_master.psel    <= 1'b1;
         apb.cb_master.penable <= 1'b0;
         apb.cb_master.pwrite  <= 1'b0;
         apb.cb_master.paddr   <= addr;
         @(apb.cb_master);
+        // Access Phase
         apb.cb_master.penable <= 1'b1;
-        do @(apb.cb_master); while (!apb.cb_master.pready);
+        //do @(apb.cb_master); while (!apb.cb_master.pready);
+        wait_ready("apb_read" , addr);
+
         data = apb.cb_master.prdata;
-        apb.cb_master.psel    <= 1'b0;
-        apb.cb_master.penable <= 1'b0;
+        $display("[APB_BFM][READ] addr=%h data=%h time=%0t",addr,data,$time);
+
+        drive_idle();
     endtask
 
 endmodule
